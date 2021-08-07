95c78c8101ff4480834158feca725161fc90709d
Shanker Karra <shankerkarra@gmail.com>
2021-08-07 11:09:50 -0600

    check in

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,16 +1,23 @@
 <template>
   <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
-    <Login />
-    <PostThread :posts="posts" />
-  </div>
-  <button type="button" value="${prev}" @click="counter-=1" class="prev">
-    Prev +  '${prev}'
-  </button>
-  <!-- <span v-for="n in Posts" :key="n.id">{{ n }} </span> -->
-  <button type="button" value="prev" @click="counter +=1" class="next">
-    Next
-  </button>
-  <div>
+    <div class="row w-80">
+      <div class="col">
+        <PostThread :posts="posts" />
+        <button type="button" value="${prev}" @click="counter-=1" class="prev">
+          Prev +  '${prev}'
+        </button>
+        <button type="button" value="prev" @click="counter +=1" class="next">
+          Next
+        </button>
+      </div>
+      <div class="col Border-black">
+        <p>
+          <Display />
+        </p>
+      </div>
+      <div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -19,7 +26,7 @@ import { computed, onMounted } from '@vue/runtime-core'
 import { AppState } from '../AppState'
 import Pop from '../utils/Notifier'
 import { postsService } from '../services/PostsService'
-// import { displaysService } from '../services/DisplayService'
+import { displayService } from '../services/DisplayService'
 
 export default {
   name: 'Home',
@@ -29,17 +36,14 @@ export default {
     onMounted(async() => {
       try {
         await postsService.getAll()
-        // await displaysService.getAll()
       } catch (error) {
         Pop.toast(error, 'error')
       }
     })
-
     return {
       prev: computed(() => AppState.posts.newer),
-      // next: computed(() => AppState.posts.older),
+      next: computed(() => AppState.posts.older),
       posts: computed(() => AppState.posts)
-      // console.log(prev + "-"+ next)
     }
   }
 }
